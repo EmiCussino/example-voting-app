26dc855f581b9dcf538f9267d37b9a050b33e67e
EmiCussino <1006633@ucc.edu.ar>
2021-11-16 17:49:18 -0300

    Ultimos cambios en test de nuevo

--- tests/voting_app_test.js
+++ tests/voting_app_test.js
@@ -8,8 +8,9 @@ Scenario('Ver opciones en pantalla de votos', ({ I }) => {
 
 Scenario('Ver opciones en pantalla de resultados', ({ I }) => {
     I.amOnPage('http://localhost:5001/');
-    I.see('CATS');
     I.see('DOGS');
+    I.see('CATS');
+    
 });
 
 Scenario('Titulo por defecto', ({ I }) => {
